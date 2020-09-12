8397c3a806c7bf4da5faf816c4bc34cbd2e397ff
soominJung0413 <doli061214@gmail.com>
2020-09-13 01:55:15 +0900

    파일 업로드 및 삭제, 썸네일 다운로드 학습

--- fileUpload/src/main/java/me/soomin/controller/UploadController.java
+++ fileUpload/src/main/java/me/soomin/controller/UploadController.java
@@ -3,6 +3,8 @@ package me.soomin.controller;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -10,13 +12,18 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -45,6 +52,91 @@ public class UploadController {
 
 		String str = sdf.format(date);
 		return str.replace("-", File.separator);
+	}
+
+	@PostMapping("/deleteFile")
+	public ResponseEntity<String> deleteFIle(String fileName, String type) {
+		log.info("deleteFile: " + fileName);
+
+		File file;
+		try {
+			file = new File("C:\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));
+
+			file.delete();
+
+			if (type.equals("image")) {
+				String largeFileName = file.getAbsolutePath().replace("s_", "");
+
+				log.info("largeFileName: " + largeFileName);
+				file = new File(largeFileName);
+
+				file.delete();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<String>("deleted", HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, String fileName) {
+		log.info("download file : " + fileName);
+
+		Resource resource = new FileSystemResource("C:\\upload\\" + fileName);
+
+		if (resource.exists() == false) {
+			return new ResponseEntity<Resource>(HttpStatus.NOT_FOUND);
+		}
+		log.info("resource : " + resource);
+
+		String resourceName = resource.getFilename();
+
+		String resourceOriginalName = resourceName.substring(resourceName.indexOf("_") + 1);
+
+		HttpHeaders headers = new HttpHeaders();
+		try {
+			String downloadName = null;
+
+			if (userAgent.contains("Trident")) {
+				log.info("IE browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8").replaceAll("\\+", " ");
+			} else if (userAgent.contains("Edge")) {
+				log.info("Edge browser");
+				downloadName = URLEncoder.encode(resourceOriginalName, "UTF-8");
+			} else {
+				downloadName = new String(resourceOriginalName.getBytes("UTF-8"), "ISO-8859-1");
+			}
+
+			headers.add("Content-Disposition", "attachment; fileName=" + downloadName);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<Resource>(resource, headers, HttpStatus.OK);
+	}
+
+	@GetMapping("/display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getFile(String fileName) {
+		log.info("fileName : " + fileName);
+
+		File file = new File("C:\\upload\\" + fileName);
+
+		log.info(file);
+
+		ResponseEntity<byte[]> result = null;
+
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-Type", Files.probeContentType(file.toPath()));
+
+			result = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
 	}
 
 	@PostMapping(value = "/uploadAjaxAction", produces = { MediaType.APPLICATION_JSON_VALUE })
--- fileUpload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ fileUpload/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -15,9 +15,54 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <title>Insert title here</title>
+
+<style type="text/css">
+.uploadResult{
+	width: 100%;
+	background-color: gray;
+}
+.uploadResult ul{
+	display: flex;
+	flex-flow: row;
+	justify-content: center;
+	align-items: center;
+}
+.uploadResult ul li{
+	list-style: none;
+	padding: 10px;
+}
+.uploadResult ul li img{
+	width: 100px;
+}
+.uploadResult ul li span{
+	color: white;
+}
+.bigPictureWrapper{
+	position: absolute;
+	display: none;
+	justify-content: center;
+	align-items: center;
+	top: 0%;
+	width: 100%;
+	height: 100%;
+	background-color: gray;
+	z-index: 100;
+	background:rgba(255,255,255,0.5);
+}
+.bigPicture img{
+	width: 600px;
+}
+</style>
 </head>
 
 <script type="text/javascript">
+function showImage(fileCallPath){
+	$(".bigPictureWrapper").css("display","flex").show();
+	$(".bigPicture")
+	.html("<img src='/display?fileName="+encodeURI(fileCallPath)+"'>")
+	.animate({width:'100%', height:'100%'},1000);
+}
+
 $(function(){
 	var regex = new RegExp("(.?)\.(exe|sh|zip|alz)$");
 	var maxSize = 5242880;
@@ -34,6 +79,8 @@ $(function(){
 		}
 		return true;
 	}
+	
+	var uploadDiv = $(".uploadDiv").clone();//파일업로드 후 초기화 하기 위함
 	
 	$("#uploadBtn").on("click",function(e){
 		var formData = new FormData();
@@ -61,9 +108,65 @@ $(function(){
 			dataType:"json",
 			success:function(result, status, xhr){
 				console.log(result);
+				
+				$(".uploadDiv").html(uploadDiv.html());
+				showUploadedFile(result);
+				
 			}
 		});
 	});
+	
+	var uploadResult = $(".uploadResult ul");
+	
+	function showUploadedFile(uploadedResultArr){
+		var str = "";
+		
+		$(uploadedResultArr).each(function(i, obj){
+			if(!obj.image){
+				var fileCallPath = encodeURIComponent(obj.uploadPath+"/"+obj.uuid+"_"+obj.fileName);
+				
+				var fileLink = fileCallPath.replace(new RegExp(/\\/g),"/");
+				
+				
+				str += "<li><div><a href='/download?fileName="+fileCallPath+"' >"+"<img src='/resources/img/attach.jpg' />"+obj.fileName+"</a>"+
+						"<span data-file=\'"+fileLink+"\' data-type=\'file\'> x </span></div></li>"; 
+			}else{
+				var fileCallPath = encodeURIComponent(obj.uploadPath+"/s_"+obj.uuid+"_"+obj.fileName);
+				
+				var originPath = obj.uploadPath+"\\"+obj.uuid+"_"+obj.fileName;
+				
+				originPath = originPath.replace(new RegExp(/\\/g), "/");
+				
+				str += "<li><a href=\"javascript:showImage(\'"+originPath+"\')\" ><img src='/display?fileName="+fileCallPath+"' /></a>"+
+						"<span data-file=\'"+fileCallPath+"\' data-type=\'image\'> x </span></li>";
+			}
+		});
+		uploadResult.append(str);
+};
+
+$(".bigPictureWrapper").on("click",function(e){
+	$(".bigPicture").animate({width:'0%',height:'0%'},1000);
+	setTimeout(function(){
+		$(this).hide();
+	},1000);
+});
+
+$(".uploadResult").on("click","span",function(e){
+	var targetFile = $(this).data("file");
+	var type = $(this).data("type");
+	console.log(targetFile);
+	
+	$.ajax({
+		url:'/deleteFile',
+		data:{fileName: targetFile, type:type},
+		dataType:'text',
+		type:'post',
+		success:function(result,status,xhr){
+			alert(result);
+		}
+	});
+});
+
 });
 </script>
 
@@ -72,8 +175,14 @@ $(function(){
  <div class="uploadDiv">
  	<input type="file" name="uploadFile" multiple="multiple" />
  </div>
- 	
- 	<button id="uploadBtn">Upload</button>
+ 	<div class="uploadResult">
+ 		<button id="uploadBtn">Upload</button>
+		 	<ul>
+		 	</ul>
+ 	</div>
+ 	<div class="bigPictureWrapper">
+ 		<div class="bigPicture"></div>
+ 	</div>
  	
  	<!-- <script type="text/javascript">
  		$(document).ready(function(){
